d926bd3da10a71d59ab9f4d2b863378a3cc35e21
Daniel Bernal <daniel.bernal@asesormasmovil.es>
2023-08-28 13:33:06 -0300

    add yabai stacked per space

--- config/.config/yabai/event_create_window.sh
+++ config/.config/yabai/event_create_window.sh
@@ -1,21 +1,34 @@
-#! /usr/bin/env sh
+#!/bin/bash
+set -a
 
-# make this file executable using:
-#     chmod +x /path/to/this/script
-# create a signal using: 
-#     yabai -m signal --add event=window_created action="/path/to/this/script"
-
-# get the window id of the newly created window
 xx=$(yabai -m query --windows --window)
-wid="$(echo $xx | jq -re ".id")"
+ss=$(yabai -m query --spaces --space)
 
-# if the split is horizontal, toggle it to vertical
-# yabai -m query --windows --window "${wid}" --stack weast | jq -re '.split == "horizontal"' \
-#    && yabai -m window ${wid} --stack $(yabai -m query --windows --window | jq -r '.id')
-# yabai -m query --windows --window "${wid}" --stack ${wid} | jq -re '.split == "horizontal"' \
-    #  && yabai -m window "${wid}" --toggle split
-# yabai -m window "${wid}" --stack "${wid}" || (yabai -m window ${wid} --toggle float && yabai -m window ${wid} --toggle float)
-# yabai -m window weast --stack $(yabai -m query --windows --window | jq -r '.id')
-#  yabai -m window north --stack $(yabai -m query --windows --window | jq -r '.id') && echo $wid && echo $(yabai -m query --windows --window | jq -r '.id')
-# yabai -m window "${wid}" --stack recent
-# yabai -m window --toogle split
+YABAI_WINDOW_ID="$(echo $xx | jq -re ".id")"
+YABAI_SPACE_ID="$(echo $xx | jq -re ".space")"
+
+WINDOWS_ARRAY=$(yabai -m query --windows --space 2 \
+| jq -re 'map(select(."is-minimized"==false and ."is-floating"==false))')
+
+# Get the number of windows on the current space
+NUMBER_OF_WINDOWS=$(echo $WINDOWS_ARRAY | jq -re 'length')
+echo "NUMBER_OF_WINDOWS: ${NUMBER_OF_WINDOWS}"
+
+if [ "$NUMBER_OF_WINDOWS" -le "4" ]; then
+   yabai -m window $YABAI_WINDOW_ID --insert stack
+   echo $ss | jq -re '.windows | join(" ")' > $HOME/.config/yabai/YABAI_INDEX_STACK_WINDOW_$YABAI_SPACE_ID
+else
+   dir=( $(cat $HOME/.config/yabai/YABAI_INDEX_STACK_WINDOW_$YABAI_SPACE_ID) )
+   dir_index=$((NUMBER_OF_WINDOWS%4))
+   if [ "${dir_index}" = "-1" ]; then
+      dir_index=0;
+   fi
+
+   if [ "${dir_index}" -ge "4" ]; then
+      dir_index=3;
+   fi
+
+   yabai -m window $YABAI_WINDOW_ID --stack ${dir[$dir_index]]}
+   echo "spliter $dir_index  ${dir[$dir_index]}"
+
+fi
